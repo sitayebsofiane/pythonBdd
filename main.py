8e4d4746167d458042573ebf89acdae84854e59c
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-01-16 20:47:57 +0100

    fix typo

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from modele.modele import  *
 from views.view import  *
 from controllers.controller import  *
 
+if __name__ == "__main__":
 
+    model=ConnectToBdd()
+    view=View(model)
+    controller=Controller(model,view)
 
-model=ConnectToBdd()
-view=View(model)
-controller=Controller(model,view)
-
-controller.begin()
-controller.dispatcher()
+    controller.begin()
+    controller.dispatcher()
